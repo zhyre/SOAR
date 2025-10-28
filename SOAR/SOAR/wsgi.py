f1fcda2625fd4ad0270e3930baaadca1b3956af7
LAB23_USER ON WS18 <L23Y15W18@LabHCI.net>
2025-10-28 17:00:36 +0800

    set wsgi manage

--- SOAR/SOAR/wsgi.py
+++ SOAR/SOAR/wsgi.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 
 import os
 import sys
-
+    
 from django.core.wsgi import get_wsgi_application
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
